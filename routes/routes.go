63ad5cf8e5a24679b724ee73d368876dac6c3779
rzhbadhon <rhbadhon030@gmail.com>
2025-10-15 23:44:37 +0600

    added new routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,4 +5,13 @@ import "github.com/gin-gonic/gin"
 func SetupRoutes(router *gin.Engine){
 	router.POST("/signup", SignUp())
 	router.POST("/login", Login())
+
+	protected := router.Group("/")
+
+	protected.Use(middleware.Authenticate())
+
+	{
+		protected.GET("users", controllers.GetUsers())
+		protecteD.GET("/user/:id", controllers.GetUser())
+	}
 }
